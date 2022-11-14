37ab7674670403cf9f3d45a006aa4d4c047234bd
Ireanuoluwa <64424508+Ireanuoluwa@users.noreply.github.com>
2022-11-14 15:11:19 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
+
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
